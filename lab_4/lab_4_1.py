532b1ff522e19675cce339e237b3053edd529204
ShiryaevNikolay <deendeeps@list.ru>
2020-12-05 13:41:03 +0300

    Лабораторная работа 4: Задание 1: пункт 3: Методом Неймана сформировать выборку величин, распределенных по рэлеевскому закону заданным б

--- lab_4/lab_4_1.py
+++ lab_4/lab_4_1.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 
 def input_use(text):
@@ -16,6 +17,14 @@ def find_xi(a, b, ri):
     return ri * (b - a) + a
 
 
+def find_max(g):
+    return (1 / g) * math.exp(-1 / 2)
+
+
+def q(g, X):
+    return X * math.exp(-X**2 / 2 * g**2)
+
+
 r = []
 for i in range(6000):
     # Получаем случайные знамения на интервале (0,1) и заполняем массив r
@@ -28,7 +37,7 @@ while a >= b:
     a, b = input_use(f"Введите интервал (a, b), при чем a<b. Пример: 6 12\n")
     if a >= b:
         print("Некорректный ввод.")
-m, d = input_use(f"Введите мат. ожидание и ср. кв. отклонение. Пример: 6 12\n")
+m, g = input_use(f"Введите мат. ожидание и ср. кв. отклонение. Пример: 6 12\n")
 
 '''Задание 1.1: Метод обратных функций'''
 # print(a, b)
@@ -45,10 +54,19 @@ for i in range(0, len(r)-5, 6):
     v = 0
     for j in range(i, i+6):
         v += r[j]
-    xi = (v - m) / d
+    xi = (v - m) / g
     x2.append(xi)
 print(len(x2))
 
 '''Задание 1.3: Метод Неймона'''
 x3 = []
-
+M = find_max(g)
+i = 0
+while len(x3) < 1000 and i < len(r) - 1:
+    print(len(x3))
+    print(i)
+    X = a + (b - a) * r[i]
+    if M * r[i + 1] < q(g, X):
+        x3.append(X)
+    i += 1
+print(len(x3))
